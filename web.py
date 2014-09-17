a2afc6eaa2294b0c6e9db1db33a53048d64d9c89
johannes-gehrs <jgehrs@gmail.com>
2014-09-17 02:32:46 +0200

    basic web frontend sceleton. JS and CSS libraries. Basic Flask view which displays a template.

--- web.py
+++ web.py
@@ -1,11 +1,11 @@
 from __future__ import absolute_import, division, unicode_literals
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('base.html')
 
 if __name__ == '__main__':
     app.debug = True
